a9f6976365d7c6d5f7358d54366fa13dbd000202
Liam White <liamthomaswhite1@gmail.com>
2018-08-12 17:26:05 +0100

    initial commit

---
+++ src/containers/home/Home.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+class Home extends Component {
+	render() {
+		return(
+			<div>
+        Welcome home
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = state => ({
+	...state
+});
+
+const mapDispatchToProps = dispatch => ({
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
