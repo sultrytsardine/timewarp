55086f3adc6df7182c6eed9f83afbbb7c45e3cf1
Liam White <liamthomaswhite1@gmail.com>
2018-08-20 21:06:49 +0100

    add google auth

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Home from './home/Home.jsx';
+import Login from './login/Login.jsx';
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
-				<div>
-					<Route exact path='/' component={Home} /> 
-				</div>
+				<Switch>
+					<Route path='/login' component={Login}/>
+					<Route exact path='/' component={Home} />
+					<Redirect from='/' to='/login'/>
+				</Switch>
 			</Router>
 		);
 	}
